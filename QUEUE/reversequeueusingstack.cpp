51874f7902cd8c27cff189f09fb11a0c086836b8
Karan Mishra <karan206245mishra@gmail.com>
2024-12-09 16:09:13 +0530

    committed

---
+++ QUEUE/reversequeueusingstack.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+void display(queue<int>&q){
+    int n=q.size();
+    for(int i=1;i<=n;i++){
+        int x=q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+}
+void reversequeue(queue<int>&q){
+    int n=q.size();
+    stack<int>st;
+    while(q.size()>0){
+        int x=q.front();
+        q.pop();
+        st.push(x);
+    }
+    while(st.size()>0){
+        q.push(st.top());
+        st.pop();
+    }
+    display(q);
+}
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    reversequeue(q);
+}
