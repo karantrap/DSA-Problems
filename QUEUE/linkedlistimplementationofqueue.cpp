91655b383074f10885e91242c24bd37cce676051
Karan Mishra <karan206245mishra@gmail.com>
2025-01-28 14:30:51 +0530

    Queue

---
+++ QUEUE/linkedlistimplementationofqueue.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node*next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+class Queue{
+    public:
+    Node*head,*tail;
+    int s;
+    Queue(){
+        head=tail=NULL;
+        s=0;
+    }
+    void push(int val){ //InsertAtTail
+        Node*temp=new Node(val);
+        if(s==0) head=tail=temp;
+        else{
+            tail->next=temp;
+            tail=temp;
+        }
+        s++;
+    }
+    void pop(){  //deleteAtHead
+        if(s==0){
+            cout<<"Queue is empty";
+            return;
+        }
+        head=head->next;
+        s--;
+    }
+    int front(){
+        if(s==0){
+            cout<<"Queue is empty";
+            return -1;
+        }
+        return head->val;
+    }
+    int back(){
+        if(s==0){
+            cout<<"Queue is empty";
+            return -1;
+        }
+        return tail->val;
+    }
+    void display(){
+        Node*temp=head;
+        while(temp!=NULL){
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    int size(){
+        return s;
+    }
+    bool empty(){
+        if(s==0) return true;
+        else return false;
+    }
+
+};
+int main(){
+    Queue q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    q.display();
+    q.pop();
+    q.display();
+    cout<<q.front()<<endl;
+    cout<<q.back();
+}
