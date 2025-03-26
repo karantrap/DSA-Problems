95148b07b25679265a6608c2c7194274ea23cfa3
Karan Mishra <karan206245mishra@gmail.com>
2025-03-26 16:45:33 +0530

    committed

---
+++ HEAPS/sortksortedarray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    int a[]={10,9,8,7,4,70,60,50};
+    int k=4;
+    int n=sizeof(a)/sizeof(a[0]);
+    priority_queue<int,vector<int>,greater<int>>pq;
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        pq.push(a[i]);
+        if(pq.size()>k){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+    }
+    while(pq.size()>0){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
+}
