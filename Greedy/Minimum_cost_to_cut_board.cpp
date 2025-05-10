55ee7c20562168fc7d1daa768e59d17ea3f35f52
Karan Mishra <karan206245mishra@gmail.com>
2025-05-10 09:59:13 +0530

    committed

---
+++ Greedy/Minimum_cost_to_cut_board.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int MinCost(vector<int>&x,vector<int>&y,int m,int n){
+    int hb=1;
+    int vb=1;
+    sort(x.rbegin(),x.rend());
+    sort(y.rbegin(),y.rend());
+    int v=0;
+    int h=0;
+    int cost=0;
+    while(v<x.size() && h<y.size()){
+        if(x[v]>y[h]){
+            cost+=x[v]*vb;
+            hb++;
+            v++;
+        }
+        else{
+            cost+=y[h]*hb;
+            vb++;
+            h++;
+        }
+    }
+    while(v<x.size()){
+        cost+=x[v]*vb;
+        hb++;
+        v++;
+    }
+    while(h<y.size()){
+        cost+=y[h]*hb;
+        vb++;
+        h++;
+    }
+    return cost;
+}
+int main(){
+    vector<int>x={2,1,3,1,4};
+    vector<int>y={4,1,2};
+    int m=6,n=4;
+    cout<<MinCost(x,y,m,n);
+}
