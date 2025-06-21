13cec8d1c3a79e158d1aece49be50d05548c04e5
Karan Mishra <karan206245mishra@gmail.com>
2025-06-21 08:04:10 +0530

    committed

--- DP/Remove_digits.cpp
+++ DP/Remove_digits.cpp
@@ -12,17 +12,28 @@ vector<int>getdigit(int n){
 }
 int reduce(int n,vector<int>&dp){
     if(n==0) return 0;
-    if(n==1) return 1;
+    if(n<10) return 1;
     if(dp[n]!=-1) return dp[n];
     vector<int>d=getdigit(n);
     int ans=INT_MAX;
     for(int i=0;i<d.size();i++) ans=min(ans,reduce(n-d[i],dp));
     return dp[n]=1+ans;
 }
+//  BOTTOM UP APPROACH
+int fbu(int num,vector<int>&dp){
+    dp[0]=0;
+    for(int i=1;i<10;i++) dp[i]=1;
+    for(int n=10;n<=num;n++){
+        vector<int>d=getdigit(n);
+        int result=INT_MAX;
+        for(int i=0;i<d.size();i++) result=min(result,dp[n-d[i]]);
+        dp[n]=1+result;
+    }
+    return dp[num];
+}
 int main(){
-    cout<<"Enter the Number:";
     int n;
     cin>>n;
     vector<int>dp(n+1,-1);
-    cout<<reduce(n,dp);
+    cout<<fbu(n,dp);
 }
