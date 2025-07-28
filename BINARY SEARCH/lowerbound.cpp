a047d9c18cd443ed36b8f2f8e482b70686db0b86
Karan Mishra <karan206245mishra@gmail.com>
2025-07-29 05:12:39 +0530

    committed

--- BINARY SEARCH/lowerbound.cpp
+++ BINARY SEARCH/lowerbound.cpp
@@ -1,34 +1,24 @@
-// //TIME COMPLEXITY IS O(N)
- #include<iostream>
- using namespace std;
-int main(){
-// //     int a[]={1,2,4,8,13,18,21,24};
-// //     int n=8;
-// //     int x=20;
-// //     for(int i=0;i<n;i++){
-// //         if(a[i]>x){
-// //             cout<<a[i-1];
-// //             break;
-// //         }
-// //     }
-// // } 
-
-// //TIME COMLEXITY SAME AS BINARY SEARCH
-   int a[]={1,2,4,5,6,9,13,24};
-   int n=8;
-   int x=13;
-   int low=0;
-    int high=n-1;
-   bool flag=false;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(a[mid]==x){
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int lowerBound(int a[],int n,int target){
+    int lo=0,hi=n-1;
+    bool flag=false;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(a[mid]==target){
             flag=true;
-            cout<<a[mid-1];
-           break;
-       }
-         else if(a[mid]<x) low=mid+1;
-         else high=mid-1;
+            return a[mid-1];///lower_bound
+        }
+        else if(a[mid]<target) lo=mid+1;
+        else hi=mid-1;
     }
-    if(flag==false) cout<<a[high];
-}  
+    if(flag==false) return a[hi];
+}
+int main(){
+    int a[]={1,4,6,9,12,15,18,21};
+    int n=sizeof(a)/sizeof(a[0]);
+    int target=11;
+    cout<<lowerBound(a,n,target);
+}
